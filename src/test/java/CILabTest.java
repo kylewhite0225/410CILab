c17098ce5628d7892817f856957792d9ec5c85bb
Kyle White <kylewhite0225@gmail.com>
2021-06-11 12:39:46 -0700

    added jacoco plugin and updated test cases

--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class CILabTest {
@@ -18,15 +19,32 @@ public class CILabTest {
         myString = null;
     }
 
+
+    // Empty string
+    // three pass cases
+    // capital in middle
+    // fail cases
     @Test
     public void detectCapitalUseTest1() {
-       fail("Not yet implemented");
-
+        myString.setString("Google");
+        boolean expected = true;
+        boolean actual = myString.detectCapitalUse();
+        assertEquals(expected, actual);
     }
     @Test
     public void detectCapitalUseTest2() {
-        fail("Not yet implemented");
+        myString.setString("GOOGLE");
+        boolean expected = true;
+        boolean actual = myString.detectCapitalUse();
+        assertEquals(expected, actual);
     }
 
+    @Test
+    public void detectCapitalUseTest3() {
+        myString.setString("google");
+        boolean expected = true;
+        boolean actual = myString.detectCapitalUse();
+        assertEquals(expected, actual);
+    }
 
 }
